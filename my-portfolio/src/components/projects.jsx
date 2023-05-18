34d389472cd1b13673ae6704a6ba4db7705dc6da
Ben Hur <Bhur777@gmail.com>
2023-05-18 14:33:49 -0400

    update footer

--- my-portfolio/src/components/projects.jsx
+++ my-portfolio/src/components/projects.jsx
@@ -12,19 +12,19 @@ const Projects = () => {
 					img="https://1000logos.net/wp-content/uploads/2017/08/Airbnb-logo.jpg"
 					title="Airbnb-clone"
 					link="https://github.com/BenHur28/airbnb-clone"
-					text="Airbnb-clone, is a fully functional airbnb website created by implementing the MERN tech stack. Users are able to host their own places and find/book nights."
+					text="Airbnb-clone, is a fully functional airbnb website created by implementing the MERN tech stack. Users are able to host their own places and book nights at any airbnb being hosted."
 				/>
 				<Row
 					img="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png"
 					title="Netflix-clone"
 					link="https://github.com/BenHur28/Netflix-clone"
-					text="Netflix-clone, is a clone that I created of the netflix landing page. Users are able to register, login, and buy a subscription. All of which was implemented using Firebase/Stripe/Redux."
+					text="Netflix-clone, is a clone that I created of the netflix landing page. Users are able to register, login, and buy a subscription. All of the following was implemented using Firebase/Stripe/Redux."
 				/>
 				<Row
 					img="https://i.pinimg.com/originals/c7/d7/02/c7d70249300e5473a14ded83c694d242.png"
 					title="PokeApp"
 					link="https://github.com/BenHur28/pokeapp"
-					text="PokeApp is a website that allows users to lookup basic pokemon information by making API calls."
+					text="PokeApp is a website where users can look up basic information about pokemon. PokeApp is one of the first websites I learned to create with React while also making API calls."
 				/>
 			</div>
 		</div>
